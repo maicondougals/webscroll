9fb5e8429fb5711b8918ef1b661b51002bab864f
Maicon Douglas <maiconboy11@gmail.com>
2024-01-30 15:41:33 -0300

    initial commit

---
+++ src/app/componentes/header/header.tsx
@@ -0,0 +1,24 @@
+import "./header.scss"
+
+
+
+export default function Header() {
+    return(
+    <header>
+      
+            <div className="title">
+                <h1>Inspiração</h1>
+            </div>
+            <div className="texto_principal">
+                <h1>Busque inspiração. <br />
+                        Conecte-se com a <br />
+                         <span> NATUREZA.</span></h1>
+            </div>
+            <div className="description">
+                <p>Acesse outras páginas por este link</p>
+            </div>
+        
+        
+    </header>
+    )
+}
---
+++ src/app/pagina/page.tsx
@@ -0,0 +1,12 @@
+
+import "./page.scss"
+import Header from "../componentes/header/header";
+
+export default function Home() {
+  return (
+    <main className="main">
+      <Header/>
+      <div className="planta"></div>
+    </main>
+  );
+}
